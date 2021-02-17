75f4ae257f5081ae83543fd55d6c0176a3cda68f
yiyi <1484491854a@gmail.com>
2021-02-17 10:33:54 +0900

    for循环应用折半查找算法，二分查找算法

---
+++ for_test.c
@@ -0,0 +1,81 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//在一个有序数组中查找具体的某个数字n，编写int binsearch（int x,int v[],int n）;
+//功能：在v[0]<=v[1]<=v[2]<=....<=v[n-1]的数组中查找x
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	//写一个代码，在arr数组（有序的）中找到7
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了，下标是:  %d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到\n");
+//	return 0;
+//}
+
+//折半查找算法/二分查找算法
+//int main() 
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//
+//	int sz = sizeof(arr) / sizeof(arr[0]);//计算元素个数
+//	int left = 0;      //左下标
+//	int right = sz - 1;   //右下标
+	
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是：%d\n", mid);
+//			break;
+//		}
+//	}
+//	if (left > right) 
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+
+//如果实现一个二分查找函数：
+//int bin_search(int arr[], int left, int right, int key)
+//{
+//	int mid = 0;
+//	while (left <= right)
+//	{
+//		mid = (left + right)>>1;
+//		if (arr[mid] > key)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < key)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			return mid;  //找到了，返回下标
+//		}
+//	}
+//	return -1;  //找不到
+//}
